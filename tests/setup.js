6d068dd0669c074ee8903966d8d134e62b3ef5ce
Thinh97 <nvq.thinh.hcmus.220597@gmail.com>
2020-08-21 12:56:19 +0700

    set default time for each test up to 30s

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,4 @@
-jest.setTimeout(10000); // set default time for each test is 10 seconds
+jest.setTimeout(30000); // set default time for each test is 10 seconds
 require('dotenv').config();
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
