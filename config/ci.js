5d94e4247409239da2bbad72193ecbaadd389987
Thinh97 <nvq.thinh.hcmus.220597@gmail.com>
2020-08-21 12:52:36 +0700

    configure .travis.yml to handle CI

--- config/ci.js
+++ config/ci.js
@@ -1,7 +1,7 @@
 module.exports = {
 	googleClientID: '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
 	googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-	mongoURI: process.env.MONGO_URI,
-	redisUrl: 'redis://127.0.0.1:6379',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_everyone',
+  redisUrl: 'redis://127.0.0.1:6379',
 	cookieKey: '123123123'
 };
--- services/cache.js
+++ services/cache.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const { promisify } = require('util');
+const keys = require('../config/keys');
 
-const redisUrl = 'redis://127.0.0.1:6379';
 const exec = mongoose.Query.prototype.exec;
-const client = redis.createClient(redisUrl, { password: process.env.REDIS_PASSWORD });
+const client = redis.createClient(keys.redisUrl, { password: process.env.REDIS_PASSWORD });
 client.hget = promisify(client.hget);
 
 mongoose.Query.prototype.cache = function(options = {}) {
